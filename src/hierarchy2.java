4e28e00b5dbc3f02099121dc7ac7c821259ed3b6
Abdulaziz2888 <164225391+Abdulaziz2888@users.noreply.github.com>
2024-05-13 22:38:32 +0600

    Update hierarchy2.java

--- src/hierarchy2.java
+++ src/hierarchy2.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class hierarchy2 {
+class Child1 extends Parent implements Printable {
+    String name;
+    int age;
+
+    public Child1(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Child1: ");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
 }

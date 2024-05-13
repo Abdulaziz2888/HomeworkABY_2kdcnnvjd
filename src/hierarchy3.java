5786606771ddd48cabcf87f38d6b10b1651d76a3
Abdulaziz2888 <164225391+Abdulaziz2888@users.noreply.github.com>
2024-05-13 22:38:55 +0600

    Update hierarchy3.java

--- src/hierarchy3.java
+++ src/hierarchy3.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class hierarchy3 {
+class Child2 extends Parent implements Printable {
+    String address;
+
+    public Child2(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Адрес: ");
+        System.out.println("Address: " + address);
+    }
 }

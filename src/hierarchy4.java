4db2b23be82a87b8afd37c7bd607b89380ab0053
Abdulaziz2888 <164225391+Abdulaziz2888@users.noreply.github.com>
2024-05-13 22:39:28 +0600

    Update hierarchy4.java

--- src/hierarchy4.java
+++ src/hierarchy4.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class hierarchy4 {
+class Child3 extends Parent implements Printable {
+    String profession;
+
+    public Child3(String profession) {
+        this.profession = profession;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Профессия: ");
+        System.out.println("Profession: " + profession);
+    }
 }
